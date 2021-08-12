4fd0e3d52f24c3610732b8f930633d7ac6ffb1a3
chingyanli <32818922+chingyanli@users.noreply.github.com>
2021-08-12 17:04:46 +0800

    Add files via upload
    [Revision] Cancel the "SOV" parameter and embed it directly into the function.

--- TISADC_DataConver_v1.1.py
+++ TISADC_DataConver_v1.1.py
@@ -22,8 +22,8 @@ def SNDataCrawler():
         print(SerialNumber[1], end=',')
     print()
 
-#================================== DuringSampling_Data Crawler ==================================
-def DataCrawler(TestItem, SOV, EOV):    #TestItem=> Testlog的關鍵字, SOV=> start_of_value, EOV=> enf_of_value   #示範(DuringSampling = , 17, 2)
+#================================== Data Crawler ==================================
+def DataCrawler(TestItem, EOV):    #TestItem=> Testlog的關鍵字, EOV=> enf_of_value(你所需要的資料長度)   #示範(DuringSampling = , 2)
 
     files = os.listdir(path)  # 得到資料夾下的所有檔名稱
     for file in files:
@@ -32,7 +32,7 @@ def DataCrawler(TestItem, SOV, EOV):    #TestItem=> Testlog的關鍵字, SOV=> s
                 text = f.read()
                 idFilter = '%s' %(TestItem)  # 找尋關鍵字
                 idPosition = text.find(idFilter)
-                start_of_value = idPosition + (SOV)
+                start_of_value = idPosition + (len(TestItem))
                 end_of_value = start_of_value + (EOV)
                 TestItem_values = text[start_of_value:end_of_value]
                 print(TestItem_values, end=',')  #確認OK
@@ -42,30 +42,30 @@ def DataCrawler(TestItem, SOV, EOV):    #TestItem=> Testlog的關鍵字, SOV=> s
 SNDataCrawler()
 
 # ========== Measurement 1 ==========
-DataCrawler('DuringSampling = ', 17, 2)
-DataCrawler('No Sampling = ', 14, 2)
-DataCrawler('BIAS_SENSOR = ', 14, 4)
-DataCrawler('REF = ', 6, 4)
-DataCrawler('VP_SENSOR = ', 12, 4)
-DataCrawler('BIST status : ', 14, 3)
+DataCrawler('DuringSampling = ', 2)
+DataCrawler('No Sampling = ', 2)
+DataCrawler('BIAS_SENSOR = ', 4)
+DataCrawler('REF = ', 4)
+DataCrawler('VP_SENSOR = ', 4)
+DataCrawler('BIST status : ', 3)
 
 # ========== Measurement 2 ==========
-DataCrawler('Offset Voltage(CH1) = ', 22, 5)
-DataCrawler('Offset Voltage(CH2) = ', 22, 5)
-DataCrawler('Offset Voltage(CH3) = ', 22, 5)
-DataCrawler('Offset Voltage(CH4) = ', 22, 5)
-DataCrawler('Offset Voltage(CH5) = ', 22, 5)
+DataCrawler('Offset Voltage(CH1) = ', 5)
+DataCrawler('Offset Voltage(CH2) = ', 5)
+DataCrawler('Offset Voltage(CH3) = ', 5)
+DataCrawler('Offset Voltage(CH4) = ', 5)
+DataCrawler('Offset Voltage(CH5) = ', 5)
 
 # ========== Measurement 3 ==========
-DataCrawler('Continuously_CH1 = ', 19, 2)
-DataCrawler('Continuously_CH2 = ', 19, 2)
-DataCrawler('Continuously_CH3 = ', 19, 2)
-DataCrawler('Continuously_CH4 = ', 19, 2)
-DataCrawler('Continuously_CH5 = ', 19, 2)
+DataCrawler('Continuously_CH1 = ', 2)
+DataCrawler('Continuously_CH2 = ', 2)
+DataCrawler('Continuously_CH3 = ', 2)
+DataCrawler('Continuously_CH4 = ', 2)
+DataCrawler('Continuously_CH5 = ', 2)
 
 # ========== Measurement 4 ==========
-DataCrawler('DUV Mode(CH1) = ', 16, 6)
-DataCrawler('DUV Mode(CH2) = ', 16, 6)
-DataCrawler('DUV Mode(CH3) = ', 16, 6)
-DataCrawler('DUV Mode(CH4) = ', 16, 6)
-DataCrawler('DUV Mode(CH5) = ', 16, 6)
+DataCrawler('DUV Mode(CH1) = ', 6)
+DataCrawler('DUV Mode(CH2) = ', 6)
+DataCrawler('DUV Mode(CH3) = ', 6)
+DataCrawler('DUV Mode(CH4) = ', 6)
+DataCrawler('DUV Mode(CH5) = ', 6)
