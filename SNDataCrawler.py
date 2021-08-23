c32f8035fde0a77362f19628274001d6e3ee75dd
chingyanli <32818922+chingyanli@users.noreply.github.com>
2021-08-23 11:56:11 +0800

    SNDataCrawler_v1.2.py
    [Release] Ok.
    修訂return功能, 為 SNDataCrawler(col_, row_)

--- SNDataCrawler.py
+++ SNDataCrawler.py
@@ -1,6 +1,6 @@
 '''撈取資料夾內所有特定欄列的資料'''
 
-def SNDataCrawler(row_):
+def SNDataCrawler(col_, row_):
     import os   # 模組 - 負責讀取資料夾跟檔案
     import csv  # 模組 - 處理 CSV
 
@@ -10,22 +10,25 @@ def SNDataCrawler(row_):
     #=================================================================================================
     #=================================== SerialNumber_Data Crawler ===================================
     #=================================================================================================
+    Means = []
+
     files = os.listdir(path)  # 得到資料夾下的所有檔名稱
     for file in files:
         #SerialNumber = file.split('.')
         #print(SerialNumber[0])  # 序號 -> 新增至sheetname
-
+        count = 0
         # =================================================================================================
         # =================================== Open CSV to recode data ====================================
         # =================================================================================================
         with open(r'%s\%s'%(path, file), encoding='utf-8', newline='') as csvfile:
             Text = csv.reader(csvfile, delimiter=',', quotechar='|')
-            Meas = []
             for row in Text:
-                Meas.append(row[row_]) #指到 CSV的 Meas欄位
-    return Meas #回傳Meas(第X欄所有內容)
+                Meas = row[col_] #指到 CSV的 Meas欄位
+                count += 1
+                if count == row_:   #指到 所需要的 資料, append到 Data的list欄位
+                    Means.append(float(Meas))
+    return Means #回傳Meas(第X欄所有內容)
 
-'''
-for i in SNDataCrawler(13):
-    print(i)
-'''
+
+#SNDataCrawler(13 , 22)
+print('SNDataCrawler(col, row)= ',SNDataCrawler(13, 22))
